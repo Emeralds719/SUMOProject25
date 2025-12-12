56f371b58ec0b9b3f6360064f44c0b588c8f1906
msreal20 <maxi18sieber05@gmail.com>
2025-12-12 12:41:52 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,16 +1,49 @@
 package hellofx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.canvas.Canvas;
 
 public class Controller {
 
     @FXML
     private Label label;
 
+    @FXML
+    private VBox simulationContainer;
+
+    @FXML
+    private Label maxSpeedLabel;
+
+    @FXML
+    private Label avgSpeedLabel;
+
+    @FXML
+    private Label connectionStatusLabel;
+
+    @FXML
+    private VBox advancedSettingsBox;
+
+    @FXML
+    private Button settingsButton;   
+
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-    }
+
+        Canvas canvas = new Canvas(380, 580);
+        simulationContainer.getChildren().add(canvas);
+
+        
+        connectionStatusLabel.setText("Status: No Live Sumo Connection");
+        connectionStatusLabel.setStyle("-fx-text-fill: red;");
+
+        connectionStatusLabel.setText("Status: Live Sumo Connection");
+        connectionStatusLabel.setStyle("-fx-text-fill: green;");
+        }
+
+        public void toggleAdvancedSettings() {
+               boolean isVisible = advancedSettingsBox.isVisible();
+               advancedSettingsBox.setVisible(!isVisible);
+               advancedSettingsBox.setManaged(!isVisible); }
 }
